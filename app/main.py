665a9352b88a7a42be766eac53cac5379e32a9f1
Young1324 <youngchou2004@gmail.com>
2024-05-06 20:23:34 +0800

    5/6/20:23

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 import os
 
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/get-image', methods=['GET'])
 def get_image():
     image_url = 'https://tse4-mm.cn.bing.net/th/id/OIP-C.KX_05K6jrKo-yU66jG3mNwHaFo?w=260&h=198&c=7&r=0&o=5&dpr=1.3&pid=1.7'
-    return jsonify({'url': image_url})
+    return image_url
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80)))
